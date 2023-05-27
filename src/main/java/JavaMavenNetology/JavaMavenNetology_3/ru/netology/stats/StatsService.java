9695fae9d31ec891129973b4d1108a6c66dd3ef4
Dmitrii-Q <120948230+Dmitrii-Q@users.noreply.github.com>
2023-05-27 15:57:27 +0300

    Update StatsService.java

--- src/main/java/JavaMavenNetology/JavaMavenNetology_3/ru/netology/stats/StatsService.java
+++ src/main/java/JavaMavenNetology/JavaMavenNetology_3/ru/netology/stats/StatsService.java
@@ -1,4 +1,64 @@
 package JavaMavenNetology.JavaMavenNetology_3.ru.netology.stats;
 
 public class StatsService {
+
+    public long sumFullSales(long[] sales) {
+        long resault = 0;
+        for (long sale : sales) {
+            resault += sale;
+        }
+        return resault;
+    }
+
+    public long sumMiddleSales(long[] sales) {
+        long resault = 0;
+        for (long sale : sales) {
+            resault += sale;
+        }
+        return resault / sales.length;
+    }
+
+    public int monthPeakSales(long[] sales) {
+        int maxMonth = 0;
+        int month = 0;
+        for (long sale : sales) {
+            if (sale >= sales[maxMonth]) {
+                maxMonth = month;
+            }
+            month = month + 1;
+        }
+        return maxMonth + 1;
+    }
+
+    public int monthMinimalSales(long[] sales) {
+        int minMonth = 0;
+        int month = 0;
+        for (long sale : sales) {
+            if (sale <= sales[minMonth]) {
+                minMonth = month;
+            }
+            month = month + 1;
+        }
+        return minMonth + 1;
+    }
+
+    public int monthBotMiddleSales(long[] sales) {
+        int resault = 0;
+        for (long sale : sales) {
+            if (sale < sumMiddleSales(sales)) {
+                resault++;
+            }
+        }
+        return resault;
+    }
+
+    public int monthUpMiddleSales(long[] sales) {
+        int resault = 0;    
+        for (long sale : sales) {
+            if (sale > sumMiddleSales(sales)) {
+                resault++;
+            }
+        }
+        return resault;
+    }
 }
